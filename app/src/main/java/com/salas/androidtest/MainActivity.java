2505378f48293e86514805bc88a56d31f2fe5bed
salas <salas@qq.com>
2018-09-18 17:38:55 +0800

    Initial commit

--- app/src/main/java/com/salas/androidtest/MainActivity.java
+++ app/src/main/java/com/salas/androidtest/MainActivity.java
@@ -2,11 +2,14 @@ package com.salas.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("文件启动了","第一个文件启动了");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
