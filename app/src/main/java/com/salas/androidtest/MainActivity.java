d0c5e3571c5d25120acbc1e503940221d875eb6d
salas <salas@qq.com>
2018-09-25 17:18:39 +0800

    Initial commit

--- app/src/main/java/com/salas/androidtest/MainActivity.java
+++ app/src/main/java/com/salas/androidtest/MainActivity.java
@@ -3,14 +3,30 @@ package com.salas.androidtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("文件启动了","第一个文件启动了");
+        Log.i("文件启动了", "第一个文件启动了");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button_1);
+        button_1Click();
+    }
+
+    public void button_1Click() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "这是弹窗", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
