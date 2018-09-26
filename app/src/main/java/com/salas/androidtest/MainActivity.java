0059402424147c42ceeb722489376c58f544e292
salas <salas@qq.com>
2018-09-26 11:03:41 +0800

    Initial commit

--- app/src/main/java/com/salas/androidtest/MainActivity.java
+++ app/src/main/java/com/salas/androidtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.salas.androidtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -28,5 +29,12 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "这是弹窗", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
     }
 }
